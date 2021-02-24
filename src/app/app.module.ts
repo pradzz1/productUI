594e238e5f81b646a9bd9ef51450ddf15bd3d8c2
Pradeep Rawat <pradzz.rawat6@gmail.com>
2021-02-24 17:02:25 +0530

    rs2form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { FilterComponent } from './filter/filter.component';
+import { FormsModule } from '@angular/forms';
 const routes: Routes = [
   {
     path: '',
@@ -26,6 +27,7 @@ const routes: Routes = [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot(routes),
+    FormsModule,
   ],
   exports: [RouterModule],
   providers: [],
--- src/app/shopping-cart/shopping-cart.component.ts
+++ src/app/shopping-cart/shopping-cart.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import data from '../../assets/data/productList.json';
 
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
   styleUrls: ['./shopping-cart.component.css']
 })
-export class ShoppingCartComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class ShoppingCartComponent  {
+  
+    /* An empty array that is responsible to add a division */
+       public items = []; 
+        public data = data.Products;
+       /* A two-way binding performed which 
+          pushes text on division */
+       public newTask; 
+     
+     
+       /* When input is empty, it will 
+          not create a new division */
+       public addToList() { 
+           if (this.newTask == '') { 
+           } 
+           else { 
+               this.items.push(this.newTask); 
+               this.newTask = ''; 
+           } 
+       } 
+     
+       /* This function takes to input the 
+          task, that has to be deleted*/
+       public deleteTask(index) { 
+           this.items.splice(index, 1); 
+       } 
 
 }
